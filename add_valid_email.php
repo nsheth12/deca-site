3ac68e7868a6fca1f5031658a271f2f2101b16fa
Nihar Sheth <nsheth12@gmail.com>
2017-05-18 17:26:29 -0400

    Added further protections to email adding textarea

--- add_valid_email.php
+++ add_valid_email.php
@@ -3,11 +3,14 @@
 require_once('./includes/connection.inc.php');
 
 if (isset($_POST["submit"])) {
-	$emails_to_add = explode("\r\n", $_POST["emails"]);
+	$emails_list = $_POST["emails"];
+	$emails_list = str_replace(" ", "\r\n", $emails_list); //replace spaces with newlines, just in case user makes an input error
+	$emails_list = str_replace(",", "\r\n", $emails_list); //replace commas with newlines, same reason
+	$emails_to_add = explode("\r\n", $emails_list);
 	$conn = dbConnect("write");
 	$sql = "INSERT INTO valid_emails (email) VALUES (?)";
 	foreach ($emails_to_add as $key => $value) {
-		$trimmed_email = trim($value);
+		$trimmed_email = strtolower(trim($value));
 		$stmt = $conn->stmt_init();
 		$stmt->prepare($sql);
 		$stmt->bind_param('s', $trimmed_email);
