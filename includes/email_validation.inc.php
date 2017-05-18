1f41ad4d14b65f94605aa604acdd41a81304e9d4
Nihar Sheth <nsheth12@gmail.com>
2017-05-18 17:33:22 -0400

    Fixed valid email system so it uses database and not text file

--- includes/email_validation.inc.php
+++ includes/email_validation.inc.php
@@ -1,14 +1,20 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/connection.inc.php");
+require_once(__DIR__ . "/connection.inc.php");
 
+//$email has already been trimmed and lowercased when passed in
 function checkIfEmailCanBeUsed ($email) {
-	$emails = file('./private/valid_emails.txt');
-	for ($i = 0; $i < count($emails); $i++){
-		if (strcasecmp($email, trim($emails[$i])) == 0) return true;
-	}
+	$conn = dbConnect("read");
+	$sql = "SELECT email FROM valid_emails WHERE email = ?";
+	$stmt = $conn->stmt_init();
+	$stmt->prepare($sql);
+	$stmt->bind_param('s', $email);
+	$stmt->execute();
+	$stmt->store_result();
+	if ($stmt->num_rows > 0) return true;
 	return false;
 }
 
+//$email has already been trimmed and lowercased when passed in
 function ensureEmailDoesNotAlreadyExist ($email){
 	$conn = dbConnect('read');
 	$sql = "SELECT email FROM users WHERE email = ? AND con_code IS NULL";
